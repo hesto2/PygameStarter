0e599dd643c50b696d0dee29e409eff21388de5a
Carter Hesterman <carter.hesterman@domo.com>
2016-05-12 18:43:15 -0600

    Menus start

--- displays/menus/startMenu.py
+++ displays/menus/startMenu.py
@@ -5,10 +5,11 @@ from entities.entity import Entity
 class StartMenu(Display):
     def __init__(self):
         font = pygame.font.Font(None, 36)
-        text = font.render("Hello There", 1, (10, 10, 10))
+        text = font.render("Start Game", 1, (255,255,255),(200,200,200))
         textpos = text.get_rect()
         textpos.centerx = C.GAME.SCREEN.get_rect().centerx
         textpos.centery = C.GAME.SCREEN.get_rect().centery *.25
         text = Entity(textpos,text)
+        
         entities = [text]
         super().__init__(C.GAME.SCREEN,entities)
